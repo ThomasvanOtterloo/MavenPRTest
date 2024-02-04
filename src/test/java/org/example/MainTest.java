0b38fcb7318472b99194b0ccbd6e42791787956b
Thomas van Otterloo <vanotterloo.thomas@gmail.com>
2024-02-04 17:21:40 +0100

    test

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+
+    }
+
+
+    // create a default test method
+    @org.junit.jupiter.api.Test
+    void test() {
+        // create a new instance of the Main class
+
+        // call the method you want to test
+
+        // compare the result with the expected result
+        assertEquals("Hello and welcome!", "Hello and welcome!");
+    }
+
+
+}
